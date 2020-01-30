958990720ee6eb5e0ebf6a93d31a168bbe978d04
Wendell Rocha <wendellrocha@outlook.com>
2020-01-30 14:16:50 -0300

    clean

--- imports/api/server/publications.js
+++ imports/api/server/publications.js
@@ -1,11 +1,6 @@
 // All links-related publications
 
 import { Meteor } from 'meteor/meteor';
-import { Links } from '../links.js';
-
-Meteor.publish('links.all', function () {
-  return Links.find();
-});
 
 Meteor.publish('teste', function () {
   var subscription = this;
--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,8 +1,5 @@
 import './home.html';
 
-import '../../components/hello/hello.js';
-import '../../components/info/info.js';
-
 Template.App_home.onCreated(function () {
     this.subscribe('teste');
 });
